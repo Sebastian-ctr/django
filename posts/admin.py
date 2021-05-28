f6296a2479eeae6e270d09daaaa3ed7d36ca7ce7
Sebastian-ctr <75362950+Sebastian-ctr@users.noreply.github.com>
2021-05-28 11:18:15 +0200

    modify admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["tittle", "update", "timestamp"]
+    list_display_links = ["update", "timestamp"]
+    search_fields = ["tittle", "content"]
+    list_filter = ["timestamp"]
+    list_editable =["tittle"]
+
+    class Meta:
+        model = Post
+
+admin.site.register(Post, PostModelAdmin)
