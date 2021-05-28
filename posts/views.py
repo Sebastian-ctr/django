8287bed656677542d124f903dc8ac7a2b29280db
Sebastian-ctr <75362950+Sebastian-ctr@users.noreply.github.com>
2021-05-28 12:36:37 +0200

    add base.html

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_home(request):
+    context = {
+        "tittle": "List"
+    }
+    return render(request, 'home.html', context)
